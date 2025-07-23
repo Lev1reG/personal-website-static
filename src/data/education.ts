cd0f89d436763e23af6d7ec0974aeb2764562d12
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-07-23 22:33:57 +0700

    fix: grammar issue in education data

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,7 +1,7 @@
 export const educationData = [
   {
     university: "Universitas Gadjah Mada",
-    degree: "Bachelor Degree in Information Engineering",
+    degree: "Bachelor's degree in Information Engineering",
     date: "2022 - 2026 (Expected)",
     href: "https://ugm.ac.id/id/prodi/teknologi-informasi/",
   },
